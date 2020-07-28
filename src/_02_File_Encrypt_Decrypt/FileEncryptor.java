5a45ef9ce977ac5ecf340ad6ce5df96521b35e19
Dexter H <dexhaehnic@gmail.com>
2020-07-27 18:28:53 -0700

    Worked on FileEncrypter

--- src/_02_File_Encrypt_Decrypt/FileEncryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileEncryptor.java
@@ -1,5 +1,10 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileEncryptor {
 	/*
 	 * Encryption is the process of encoding a message or information
@@ -20,4 +25,41 @@ public class FileEncryptor {
 	 * Create a program that takes a message and a key from the user.
 	 * Use the key to shift each letter in the users input and save the final result to a file.
 	 */
+	
+	public static void main(String[] args) {
+		int key = Integer.parseInt(JOptionPane.showInputDialog("put key here"));
+		String text = JOptionPane.showInputDialog("put text here");
+		StringBuilder sb = new StringBuilder(text);
+		char temp;
+		
+		for (int i = 0; i < text.length(); i++) {	
+			sb.replace(i, i+1, shift(sb.charAt(i), key));
+		}
+		
+		try {
+			FileWriter fw = new FileWriter("src/_02_File_Encrypt_Decrypt/file.txt");
+			fw.write(sb.toString());
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static String shift(char c, int key) {
+		char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+						   'n', 'o', 'p', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+						   'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 
+						   'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'h', 'i',
+						   'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
+						   'w', 'x', 'y', 'z'};
+		
+		for (int i = 0; i < alphabet.length; i++) {
+			if(c == ' ') {
+				return " ";
+			}else if(c == alphabet[i]) {
+				return Character.toString(alphabet[i+key]);
+			}
+		}
+		return null;
+	}
 }
